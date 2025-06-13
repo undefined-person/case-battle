bd189ac5de04b73258d832581cec504d3ce75602
Anastasia <nastiakarachova@gmail.com>
2025-06-13 21:16:05 +0000

    Implement wallet connection button in Header component: add state management for button visibility, update button functionality, and adjust balance display.

--- src/shared/components/header/header.jsx
+++ src/shared/components/header/header.jsx
@@ -1,5 +1,6 @@
 import Avatar from '../../assets/images/avatar.png'
 import StarIcon from '../../assets/icons/star.svg?react'
+import { useState } from 'react'
 
 import styles from './header.module.scss'
 import { useTranslation } from 'react-i18next'
@@ -8,25 +9,28 @@ import TonIcon from '../../assets/icons/ton.svg?react'
 
 export default function Header({ className }) {
   const { t } = useTranslation()
+  const [isShowConnectWallet, setIsShowConnectWallet] = useState(false)
   return (
     <header className={className}>
-      {/* <div className={styles.balance_container}>
+      <div className={styles.balance_container}>
         <div className={styles.user_info}>
           <img src={Avatar} alt="avatar" className={styles.avatar} />
           <h3>{t('profile.title')}</h3>
         </div>
-        <button className={styles.balance_button}>
+        <button className={styles.balance_button} onClick={() => setIsShowConnectWallet(true)}>
           <div className={styles.balance_info}>
             <StarIcon className={styles.star} />
-            <span>205</span>
+            <span>0</span>
           </div>
           <span className={styles.plus}>+</span>
         </button>
-      </div> */}
-      <Button>
-        {t('connect_to_wallet')}
-        <TonIcon />
-      </Button>
+      </div>
+      {isShowConnectWallet ? (
+        <Button className={styles.connect_wallet_button}>
+          {t('connect_to_wallet')}
+          <TonIcon />
+        </Button>
+      ) : null}
     </header>
   )
 }
